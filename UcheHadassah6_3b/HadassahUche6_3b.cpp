7e75a6e7858deadefea0d0cdef789e55e2079fab
Hadassah Uche <uhadassah1@gmail.com>
2023-11-19 23:17:25 +0200

    Cleaned up the code and added relevant comments

--- UcheHadassah6_3b/HadassahUche6_3b.cpp
+++ UcheHadassah6_3b/HadassahUche6_3b.cpp
@@ -1,12 +1,11 @@
-/*b) Re-write the above exercise with a function: Write a function called IsPalindrome 
-which takes as a parameter a string and returns true (i.e. 1) if the string is a 
-palindrome. Write a main program to test your function with a user-defined string. 
-This program does not count upper case and lower case as equal*/
+/*Name:Uche Hadassah
+This program checks if a word is a palindrome but makes use of the IsPalindrome function 
+NB:This program does not count upper case and lower case as equal*/
 #include<iostream>
 #include<cstring>
 using namespace std;
 const int MAX = 100;
-bool IsPalindrome(char[]);
+bool IsPalindrome(char[]);//Function prototype
 int main()
 {
 	char word[MAX];
@@ -28,18 +27,18 @@ bool IsPalindrome(char word[])
 	for (j = 0; word[j] != '\0'; j++);//Gets the length of the word
 	while (i <= j - 1)
 	{
-		if (word[i] != word[j - 1])
+		if (word[i] != word[j - 1])//If there is a slight difference in the first loop
 		{
 			return false;
 		}
-		if (i == j - 1)
+		if (i == j - 1)//When it reaches the end and they are both equal
 		{
 			return true;
 		}
 		i++;
 		j--;
 	}
-	if (i > j - 1)
+	if (i > j - 1)//Incase we have an even number of alphabets in a word
 	{
 		return true;
 	}
