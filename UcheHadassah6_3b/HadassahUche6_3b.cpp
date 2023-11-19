6169b88b7daaf78affd15f716873239875d20fe6
Hadassah Uche <uhadassah1@gmail.com>
2023-11-19 23:13:19 +0200

    I think I'm pretty much done with implementing the function.

--- UcheHadassah6_3b/HadassahUche6_3b.cpp
+++ UcheHadassah6_3b/HadassahUche6_3b.cpp
@@ -1,6 +1,7 @@
 /*b) Re-write the above exercise with a function: Write a function called IsPalindrome 
 which takes as a parameter a string and returns true (i.e. 1) if the string is a 
-palindrome. Write a main program to test your function with a user-defined string. */
+palindrome. Write a main program to test your function with a user-defined string. 
+This program does not count upper case and lower case as equal*/
 #include<iostream>
 #include<cstring>
 using namespace std;
@@ -9,34 +10,38 @@ bool IsPalindrome(char[]);
 int main()
 {
 	char word[MAX];
-	int i = 0, j;
 	cout << "Enter a word:";
 	cin >> word;
-	//I will add a condition to take a word not a string
-	
+	if (IsPalindrome(word))
+	{
+		cout << "This is a palindrome!";
+	}
+	else
+	{
+		cout << "This is not a palindrome!";
+	}
 }
 
-bool IsPalindrome(char[])
+bool IsPalindrome(char word[])
 {
-	int j;
+	int i = 0, j;
 	for (j = 0; word[j] != '\0'; j++);//Gets the length of the word
 	while (i <= j - 1)
 	{
 		if (word[i] != word[j - 1])
 		{
-			cout << "This is not a palindrome";
-			break;
+			return false;
 		}
 		if (i == j - 1)
 		{
-			cout << "This is a palindrome";
+			return true;
 		}
 		i++;
 		j--;
 	}
 	if (i > j - 1)
 	{
-		cout << "This is a palindrome";
+		return true;
 	}
 	return false;
 }
